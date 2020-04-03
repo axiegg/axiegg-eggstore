0a35d0b3a775bced5f25270011a98e19b4c731d3
hydr063n <hydr063n@yandex.com>
2020-04-04 00:15:18 +0200

    On arrow click show next team, And on prev

---
+++ client/src/components/Axie/AxieTeamView/index.js
@@ -0,0 +1,45 @@
+/* eslint react/prop-types: 0 */
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import classnames from 'classnames';
+import styles from './index.module.sass';
+
+import { AxieImage, AxieName, AxieBadge } from 'components/Axie';
+import Box from 'components/Box';
+import Image from 'components/Image';
+import { getAxiePNG } from 'services/Axie';
+
+const AxieTeamView = ({
+  axie,
+  axie: {
+    id,
+    tokenId,
+    breedCount,
+    title,
+    name,
+  },
+  className,
+}) => {
+  console.log('Axie team view');
+
+  return (
+    <div>
+      <Link className={classnames(styles.axie, className)} to={`/axie/${tokenId}`}>
+        <Box>
+          <AxieName {...{ name: `#${tokenId}`, axieClass: axie.class }} />
+          <AxieBadge {...{ title }} />
+          <div className={styles.breedContainer} />
+          <Image
+            className={classnames(styles.axieImg, className)}
+            src={getAxiePNG(tokenId)}
+            alt={`Axie #${tokenId}`}
+          />
+          <p className={styles.axieName}>{name}</p>
+        </Box>
+      </Link>
+    </div>
+  );
+}
+
+export default AxieTeamView;
